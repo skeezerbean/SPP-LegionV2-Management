af764e9d94438f89a2c4e0eea22922729f458915
skeezerbean <mhyslip+z@teamstc.com>
2021-03-24 00:56:49 -0400

    0.0.2.11 Add splash screen, courtesy of SPP Discord m1 <3

---
+++ Views/SplashScreenView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Windows;
+
+namespace SPP_LegionV2_Management
+{
+	/// <summary>
+	/// Interaction logic for SplashScreen.xaml
+	/// </summary>
+	public partial class SplashScreenView : Window
+	{
+		// Graphics for splash screen designed by m1#2698 and are pretty awesome!
+		public SplashScreenView()
+		{
+			InitializeComponent();
+		}
+
+		private void Storyboard_Completed(object sender, System.EventArgs e)
+		{
+			this.Close();
+		}
+	}
+}
